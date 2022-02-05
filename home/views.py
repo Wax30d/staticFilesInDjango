3787df6eee4ed43779f0e75942794d76efa5cceb
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-02-05 21:00:23 +0500

    Added view functions.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+
+def other(request):
+    context = {
+        'k1': 'Welcome to the Second page',
+    }
+    return render(request, 'others.html', context)
